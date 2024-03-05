203c8a3bca2b57c451598d9423e70c509955315a
MarianaLeonS <160764750+MarianaLeonS@users.noreply.github.com>
2024-03-05 16:44:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
+df_yield= pd.read_csv(url)
