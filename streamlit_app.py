c0fe19990550070ee4e31f4f3695417592ffd6a8
MarianaLeonS <160764750+MarianaLeonS@users.noreply.github.com>
2024-05-13 21:09:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
+/streamlit_app
+    /pages
+        __init__.py   # This can be empty; it just marks 'pages' as a Python package directory
+        home.py       # This will contain the code for the "Home" page
+        learn.py      # This will contain the code for the "Learn" page
+        test.py       # This will contain the code for the "Test" page
+    streamlit_app.py           # This is the main app file
+
 import streamlit as st
 from pages import home, learn, test
 
