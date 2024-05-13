f2ef9f03eff8014415a3d8e00a4ad7433fbbc0d2
MarianaLeonS <160764750+MarianaLeonS@users.noreply.github.com>
2024-05-13 20:45:04 +0200

    Update streamlit_app.py
    Testing multipages

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,24 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
-df_yield= pd.read_csv(url)
-
-# Assuming df_yield is your DataFrame
-data = df_yield['Item'].value_counts().reset_index()
-data.columns = ['Item', 'count']  # Rename columns for clarity
+data = pd.read_csv('https://github.com/cvermno/ML-Project/raw/main/Datasets/training_data.csv')
 
 # Streamlit app
-st.title('Shares of crops')
+st.title('Le Petit Prof')
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
 
 # Create a pie chart using Plotly Express
-fig = px.pie(data, names='Item', values='count', title='Shares of crops')
+fig = px.pie(data, names='difficulty', values='count', title='French sentences sorted by difficulty')
 
 # Display the pie chart using Streamlit
 st.plotly_chart(fig)
-
+st.write("#Is French a difficult language to learn?")
+st.write(training)
