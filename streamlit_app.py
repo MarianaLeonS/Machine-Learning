7c521bf94973198a4f3a3f82d487a8008d567b40
MarianaLeonS <160764750+MarianaLeonS@users.noreply.github.com>
2024-05-13 20:51:12 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-data = pd.read_csv('https://github.com/cvermno/ML-Project/raw/main/Datasets/training_data.csv')
-
-# Streamlit app
-st.title('Le Petit Prof')
 
 st.set_page_config(
     page_title="Hello",
@@ -15,11 +8,3 @@ st.set_page_config(
 st.write("# Welcome to Streamlit! 👋")
 
 st.sidebar.success("Select a demo above.")
-
-# Create a pie chart using Plotly Express
-fig = px.pie(data, names='difficulty', values='count', title='French sentences sorted by difficulty')
-
-# Display the pie chart using Streamlit
-st.plotly_chart(fig)
-st.write("#Is French a difficult language to learn?")
-st.write(training)
